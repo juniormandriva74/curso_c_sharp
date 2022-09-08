a85a821183c76921186dd7c36e2c699fa228d67a
Junior Santos <junior.mandriva74@gmail.com>
2022-09-08 16:20:17 -0300

    Segundo commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Calcular a idade de uma pessoa.
+Console.Write("Digite o seu nome: ");
+string name = Console.ReadLine();
+
+Console.Write("Digite o ano do seu nascimento: ");
+int year = int.Parse(Console.ReadLine());
+
+int age = 2022 - year;
+
+Console.WriteLine($"Olá {name}, a sua idade é {age} anos.");
+
+if (age > 17) Console.WriteLine("Você é maior de idade.");
+else Console.WriteLine("Você é menor de idade.");
+
+

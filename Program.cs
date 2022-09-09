0bb93964001be5e8eacf5a3899ac586962edabc3
Junior Santos <junior.mandriva74@gmail.com>
2022-09-09 11:18:44 -0300

    Estudos de classes e excessões

--- Program.cs
+++ Program.cs
@@ -1,5 +1,30 @@
-﻿// Calcular a idade de uma pessoa.
-Console.Write("Digite o seu nome: ");
+﻿class Program
+{
+    static void Main()
+    {
+        // Arrays
+        string[] names = { "Matheus", "Lindinalva", "Valdeci" };
+
+        foreach (string name in names)
+        {
+            Console.WriteLine(name);
+        }
+
+        /*
+        for (int i = 0; i < names.Length; i++)
+        {
+            Console.WriteLine(names[i]);
+        }
+        */
+    }
+}
+
+
+
+
+
+// Calcular a idade de uma pessoa.
+/*Console.Write("Digite o seu nome: ");
 string name = Console.ReadLine();
 
 Console.Write("Digite o ano do seu nascimento: ");
@@ -9,7 +34,10 @@ int age = 2022 - year;
 
 Console.WriteLine($"Olá {name}, a sua idade é {age} anos.");
 
-if (age > 17) Console.WriteLine("Você é maior de idade.");
-else Console.WriteLine("Você é menor de idade.");
-
-
+if (age > 17) {
+    Console.WriteLine("Você é maior de idade.");
+} 
+else {
+    Console.WriteLine("Você é menor de idade.");
+} 
+*/

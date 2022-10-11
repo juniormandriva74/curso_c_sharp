f56638ead85a85d822eec1fea703d4d0bf626aef
Junior Santos <junior.mandriva74@gmail.com>
2022-10-11 15:03:00 -0300

    Aulas 05 e 06

--- Program.cs
+++ Program.cs
@@ -1,11 +1,40 @@
 ﻿using System;
 
 class Principal {
-    static void Main() {
-        byte num1 = 10;
-        string nome = "José";
 
-        Console.WriteLine(num1);
-        Console.WriteLine(nome);
+    static void Main() {
+
+        // Aula 06 - Formatando a saída no console
+        // Usando formatações especiais para porcentagem e valor monetário
+
+        double valorCompra=105.50;
+        double valorVenda;
+        double lucro=0.1;
+        string produto="memoria ram";
+
+        valorVenda=valorCompra+(valorCompra*lucro);
+
+        Console.WriteLine("Produto........:{0,15}",produto);
+        Console.WriteLine("Val. Compra....:{0,15:c}",valorCompra);
+        Console.WriteLine("Lucro..........:{0,15:p}",lucro);
+        Console.WriteLine("Val. Venda.....:{0,15:c}",valorVenda);
+
+            //int n1, n2, n3;
+
+            //n1=10; n2=20; n3=30;
+
+            //Console.WriteLine("N1=" +n1+ " N2=" +n2+ " N3=" +n3);
+            //Console.WriteLine("N1={0}, N2={1}, N3={2}",n1,n2,n3);
+            //Console.WriteLine($"N1={n1}, N2={n2}, N3={n3}");
+
+
+         //Aula 05 - Operadores e operações
+         /* //int num=10;
+        //num=num+1;
+        //num+=1;
+
+        bool res = (5<7);
+
+        Console.WriteLine(res); */
     }
 }
